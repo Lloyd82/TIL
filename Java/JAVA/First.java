cc0db13c5da21ea950b8f4cb111fc5eab1a72858
Lloyd82 <within54@gmail.com>
2021-10-09 22:23:46 +0900

    add day study & fixed

--- Java/JAVA/First.java
+++
@@ -1,6 +0,0 @@
-class First{
-	public static void main(String[] args){
-		String str="Java 수업입니다.";
-		System.out.println(str);
-	}
-}
--- Java/JAVA/workspace/javatest/src/pa/A3.java
+++
@@ -1,8 +0,0 @@
-package pa;
-
-    public class A3 {
-  int i;
-  protected int pro;
-  private int pri;
-  public int pub;
-   }
